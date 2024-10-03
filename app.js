75beeb68cd545f84e63a261d8a8f5b775e58fd08
Danish <itsaws786@gmail.com>
2024-10-03 11:25:23 +0530

    episode 1

---
+++ app.js
@@ -0,0 +1,17 @@
+const parent = React.createElement('div', { id: "parent" },
+    React.createElement("div", { id: "child" },
+        [
+            React.createElement('h1', {}, "i am h1 tag"),
+            React.createElement('h2', {}, "i am h2 tag")
+        ]
+    ))
+
+//JSX - as the above code will become cluttered if it is long
+//React is only written using JSX
+
+
+const heading = React.createElement("h1", { id: "heading", xyz: "danish" }, "Heading from Danish")
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(parent)
