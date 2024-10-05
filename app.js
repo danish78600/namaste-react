57146576619f9676e0297c36d54b54aa1f54e379
Danish <itsaws786@gmail.com>
2024-10-05 11:32:35 +0530

    episode 3 completed

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const parent = React.createElement('div', { id: "parent" },
-    React.createElement("div", { id: "child" },
-        [
-            React.createElement('h1', {}, "i am h1 tag"),
-            React.createElement('h2', {}, "i am h2 tag")
-        ]
-    ))
 
-//JSX - as the above code will become cluttered if it is long
-//React is only written using JSX
+const title = (
+    <h1 id="heading">
+        Namaste React Series
+    </h1>
+)
+
+const HeadingComponent = () => {
+    return (
+        <div>
+            {title}
+            <h1>Namaste React Funcional Component</h1>
+        </div>
+    )
+}
 
 
-const heading = React.createElement("h1", { id: "heading", xyz: "danish" }, "Heading from Danish")
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(parent)
+root.render(<HeadingComponent/>) 
